e5d8140e9ee85070c45fe83e445ea5332ae14029
CHChang810716 <CHChang810716@gmail.com>
2019-02-05 22:27:19 +0800

    add packages

--- cmake/packages.cmake
+++ cmake/packages.cmake
@@ -1,6 +1,20 @@
 hunter_add_package(Arkitekto)
 find_package(Arkitekto CONFIG REQUIRED)
+
 if(BUILD_TEST)
     hunter_add_package(GTest)
     find_package(GTest CONFIG REQUIRED)
 endif()
+
+hunter_add_package(Boost COMPONENTS
+    system 
+    filesystem
+    serialization
+)
+find_package(Boost CONFIG COMPONENTS
+    system 
+    filesystem
+    serialization
+
+    REQUIRED
+)
