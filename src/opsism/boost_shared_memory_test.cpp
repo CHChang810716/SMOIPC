6efa2df412698e2a1e14a337a9f0e0ab54a6faf3
CHChang810716 <CHChang810716@gmail.com>
2019-02-05 22:34:35 +0800

    [WIP] workable env

---
+++ src/opsism/boost_shared_memory_test.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+#include <boost/interprocess/shared_memory_object.hpp>
+TEST(boost_shmem, basic_test) {
+
+}
--- src/opsism/fibonacci_test.cmake
+++ src/opsism/fibonacci_test.cmake
@@ -1,2 +1,4 @@
 include(cmake/scheme/test.cmake)
-target_link_libraries(${AKT_TARGET} PRIVATE CppProjectTemplate-fibonacci)
+if(BUILD_TEST)
+    target_link_libraries(${AKT_TARGET} PRIVATE opsism-fibonacci)
+endif()
