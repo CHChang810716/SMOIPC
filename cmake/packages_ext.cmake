8dea5c74eb121ac3318d90df6e86c6e2f361635f
John Chang <CHChang810716@gmail.com>
2019-02-14 03:49:44 +0800

    [WIP] fix linux build fail

--- cmake/packages_ext.cmake
+++
@@ -1,3 +0,0 @@
-if(UNIX)
-    target_link_libraries(Boost::system INTERFACE rt)
-endif()
--- cmake/scheme/basic_target_link.cmake
+++ cmake/scheme/basic_target_link.cmake
@@ -1,1 +1,4 @@
 target_link_libraries(${AKT_TARGET} PRIVATE Threads::Threads)
+if(UNIX)
+    target_link_libraries(${AKT_TARGET} PRIVATE ${CMAKE_DL_LIBS} rt)
+endif()
