57d7bea6785787e829bc359cda8adc2edb281260
John Chang <CHChang810716@gmail.com>
2019-02-14 04:02:03 +0800

    [WIP] fix linux build fail

--- test/boost/shared_memory_test.cmake
+++ test/boost/shared_memory_test.cmake
@@ -1,2 +1,4 @@
 include(cmake/scheme/test.cmake)
-target_link_libraries(${AKT_TARGET} PRIVATE Boost::system ws2_32)
+if(WIN32)
+    target_link_libraries(${AKT_TARGET} PRIVATE Boost::system ws2_32)
+endif()
