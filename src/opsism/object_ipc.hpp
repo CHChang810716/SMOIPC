50c172d6c434e01a713b3b7d8669dcad0a9270bc
John Chang <CHChang810716@gmail.com>
2019-02-20 03:38:59 +0800

    [WIP] object_ipc_test

--- src/opsism/object_ipc.hpp
+++ src/opsism/object_ipc.hpp
@@ -1,6 +1,19 @@
 #pragma once
+#include "object_ipc/receiver.hpp"
+#include "object_ipc/sender.hpp"
 namespace opsism {
+template<class... Types>
+using TypeList = boost::mpl::vector<Types...>;
 
-
+template<class Trait>
+struct ObjectIPC {
+    constexpr static auto buffer_bytes = Traits::buffer_bytes;
+    using Receiver = opsism::object_ipc::Receiver<
+        Trait::ComTypes, buffer_bytes 
+    >;
+    using Sender = opsism::object_ipc::Sender<
+        Trait::ComTypes, buffer_bytes
+    >;
+};
 
 }
---
+++ test/opsism/object_ipc_test.cpp
@@ -0,0 +1,5 @@
+#include <opsism/object_ipc.hpp>
+#include <gtest/gtest.h>
+#include "object_ipc_test/com_trait.hpp"
+TEST(opsism_object_ipc, basic_test) {
+}
